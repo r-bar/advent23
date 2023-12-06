4f03dc7540e5373c10ed906c991670dc1392c02d
Ryan Barth <ryan@barth.tech>
2023-12-05 22:35:53 -0800

    finish day06

--- day06/src/bin/d6p1.rs
+++ day06/src/bin/d6p1.rs
@@ -2,15 +2,78 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 
+#[derive(Debug, Clone, Copy)]
+struct Race {
+    time: u32,
+    distance_record: u32,
+}
+
+impl Race {
+    fn strategies(&self) -> Vec<(u32, u32)> {
+        let mut result = Vec::with_capacity(self.time as usize);
+        for hold in 0..self.time {
+            let distance = race(self.time, hold);
+            result.push((hold, distance));
+        }
+        result
+    }
+}
+
+/// Returns the distance traveled after `t` seconds, given a `hold` time.
+fn race(t: u32, hold: u32) -> u32 {
+    if hold >= t {
+        return 0;
+    }
+    let speed = hold;
+    let runtime = t - hold;
+    runtime * speed
+}
+
 fn main() -> anyhow::Result<()> {
     let filename = std::env::args()
         .nth(1)
         .unwrap_or_else(|| "input.txt".to_string());
     let f = File::open(filename)?;
     let reader = BufReader::new(f);
+    let mut times: Option<Vec<u32>> = None;
+    let mut distances: Option<Vec<u32>> = None;
     for line in reader.lines() {
-        println!("{}", line?.trim());
+        let line = line?;
+        let parts: Vec<_> = line.split_whitespace().collect();
+        match parts[0] {
+            "Time:" => {
+                times = parts[1..].iter().map(|s| s.parse::<u32>().ok()).collect();
+            }
+            "Distance:" => {
+                distances = parts[1..].iter().map(|s| s.parse::<u32>().ok()).collect();
+            }
+            _ => continue,
+        }
     }
+    let times = times.ok_or_else(|| anyhow::anyhow!("No times found"))?;
+    let distances = distances.ok_or_else(|| anyhow::anyhow!("No distances found"))?;
+    let races: Vec<_> = times
+        .into_iter()
+        .zip(distances)
+        .map(|(time, distance)| Race {
+            time,
+            distance_record: distance,
+        })
+        .collect();
+    let margin: usize = races
+        .iter()
+        .enumerate()
+        .map(|(i, r)| {
+            let winning = r.strategies()
+                .into_iter()
+                //.inspect(|(hold, distance)| { if i == 2 { dbg!(&i, hold, distance); } })
+                .filter(|(_hold, d)| d > &r.distance_record)
+                .count();
+            //dbg!(&i, winning);
+            winning
+        })
+        .product();
+    dbg!(&margin);
     Ok(())
 }
 
--- day06/src/bin/d6p2.rs
+++ day06/src/bin/d6p2.rs
@@ -2,15 +2,37 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 
+fn quadratic(time: f64, distance: f64) -> f64 {
+    let s1 = time + ((time * time) - (4.0 * distance)).sqrt() / 2.0;
+    let s2 = time - ((time * time) - (4.0 * distance)).sqrt() / 2.0;
+    s1.floor() - s2.floor()
+}
+
 fn main() -> anyhow::Result<()> {
     let filename = std::env::args()
         .nth(1)
         .unwrap_or_else(|| "input.txt".to_string());
     let f = File::open(filename)?;
     let reader = BufReader::new(f);
+    let mut time = 0.0;
+    let mut distance_record = 0.0;
     for line in reader.lines() {
-        println!("{}", line?.trim());
+        let line = line?;
+        let parts: Vec<_> = line.split_whitespace().collect();
+        match parts[0] {
+            "Time:" => {
+                let s: String = parts[1..].iter().flat_map(|s| s.chars()).collect();
+                time = s.parse::<f64>()?;
+            }
+            "Distance:" => {
+                let s: String = parts[1..].iter().flat_map(|s| s.chars()).collect();
+                distance_record = s.parse::<f64>()?;
+            }
+            _ => continue,
+        }
     }
+    let margin = quadratic(time, distance_record);
+    println!("{margin}");
     Ok(())
 }
 
@@ -21,6 +43,8 @@ mod tests {
 
     #[test]
     fn example_test() {
-        let _example = include_str!("../../example.txt");
+        let time = 71530.0;
+        let distance = 940200.0;
+        assert_eq!(quadratic(time, distance), 71503.0);
     }
 }
